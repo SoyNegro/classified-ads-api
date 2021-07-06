5be5ffae466240de7f0a701f9bf2dc5f7eae485b
SoyNegro <krisoul.valdes@gmail.com>
2021-07-06 09:50:32 -0400

    updated to match service

--- src/model/AccessCredential.kt
+++ src/model/AccessCredential.kt
@@ -1,10 +1,11 @@
 package com.thedarksideofcode.model
 
 import kotlinx.serialization.Serializable
-import org.bson.types.ObjectId
 
 @Serializable
-data class AccessCredential(val apiKey: String,
-                            val password: String,
-                            val ownerId: ObjectId,
-                            val permissions: String)
+data class AccessCredential(
+    val apiKey: String,
+    val password: String,
+    val owningUser: String,
+    val permissions: String
+)
